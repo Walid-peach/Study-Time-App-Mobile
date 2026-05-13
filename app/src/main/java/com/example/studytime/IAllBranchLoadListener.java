8f8031058408f9a888a18c4da6274670e3a6bedf
Walid-peach <walidelkhoukh99@gmail.com>
2026-05-13 23:22:08 +0200

    refactor: migrate project to Kotlin by replacing Java classes with Kotlin equivalents and modernizing the architecture.

--- app/src/main/java/com/example/studytime/IAllBranchLoadListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.studytime;
-
-import java.util.List;
-
-public interface IAllBranchLoadListener {
-    void onAllBranchloadSuccess(List<Salon> salleList);
-    void onAllBranchloadFailed(String message);
-}
--- app/src/main/java/com/example/studytime/IAllSalleLoadListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.studytime;
-
-import java.util.List;
-
-public interface IAllSalleLoadListener {
-    void onAllSalleloadSuccess(List<String> areaNameList);
-    void onAllSalleloadFailed(String message);
-}
